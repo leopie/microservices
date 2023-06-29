811714ab58463a9307eab241f89da1cee272f8be
leonardo.pierangelini <leonardo.pierangelini@gmail.com>
2023-06-29 17:33:25 +0200

    Replaced RestTemplate client with OpenFeign client

--- customer/src/main/java/com/leopie/customer/CustomerApplication.java
+++ customer/src/main/java/com/leopie/customer/CustomerApplication.java
@@ -2,8 +2,12 @@ package com.leopie.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(
+        basePackages = "com.leopie.clients"
+)
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- customer/src/main/java/com/leopie/customer/CustomerService.java
+++ customer/src/main/java/com/leopie/customer/CustomerService.java
@@ -1,12 +1,14 @@
 package com.leopie.customer;
 
+import com.leopie.clients.fraud.FraudClient;
 import com.leopie.fraud.FraudCheckResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public record CustomerService(CustomerRepository customerRepository,
-                              RestTemplate restTemplate) {
+                              RestTemplate restTemplate,
+                              FraudClient fraudClient) {
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
@@ -16,11 +18,8 @@ public record CustomerService(CustomerRepository customerRepository,
         //TODO check if email is valid
         //TODO check if email is not taken
         customerRepository.saveAndFlush(customer);
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
         if(fraudCheckResponse!=null && fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
